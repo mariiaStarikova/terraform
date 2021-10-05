805edbc95cd2a67559c754262519f45d9842de3e
mariiaStarikova <37140466+mariiaStarikova@users.noreply.github.com>
2021-10-05 12:10:10 +0300

    Update auto-test-tf-template-aws.tf

--- m3-autotest/aws-terraform/auto-test-tf-template-aws.tf
+++ m3-autotest/aws-terraform/auto-test-tf-template-aws.tf
@@ -1,14 +1,13 @@
 provider "aws" {
    region = "us-east-2"
  }
-
  resource "aws_instance" "test_VM" {
    # Amazon Linux AMI 2017.03.1 (HVM)
    ami           = "ami-657bd20a"
    instance_type = "t2.micro"
 
    tags = {
-     Name = "${var.vm_name}"
+     Name = var.vm_name
    }
  }
 
