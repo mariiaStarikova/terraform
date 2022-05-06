5cf53eae3bce828209250b3f412c7c48d73b8deb
mariiaStarikova <37140466+mariiaStarikova@users.noreply.github.com>
2022-05-06 13:11:17 +0300

    Create auto_test_tf_template_open_stack.tf

---
+++ m3-autotest/open_stack-terraform/auto_test_tf_template_open_stack.tf
@@ -0,0 +1,13 @@
+provider"m3"{}
+resource"m3_instance""my-server"{
+image="centos7_64-bit"
+instance_name=var.vm_name
+region_name=var.regionName
+tenant_name=var.tenantName
+shape="SMALL"
+owner=var.owner
+}
+variable"vm_name"{
+default="auto-test-tf-template-openstack"
+description="NameforVMtobecreated"
+}
