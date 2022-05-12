a7b036033944bf60357d18c49189b80d69b4bb90
mariiaStarikova <37140466+mariiaStarikova@users.noreply.github.com>
2022-05-12 16:39:11 +0300

    Update auto-test-tf-template-aws.tf

--- m3-autotest/aws-terraform/auto-test-tf-template-aws.tf
+++ m3-autotest/aws-terraform/auto-test-tf-template-aws.tf
@@ -5,12 +5,12 @@ provider "aws" {
    # Amazon Linux AMI 2017.03.1 (HVM)
    ami           = "ami-657bd20a"
    instance_type = "t2.micro"
-
    tags = {
      Name = var.vm_name
    }
  }
+
  variable "vm_name" {
-   default = "auto-test-multi-stack-template-git"
+   default = "auto-test-multi-stack-template-git1"
    description = "Name for VM to be created"
 }
